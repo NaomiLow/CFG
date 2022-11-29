3c91c0886717be43c3be86f8bd467e8bbc18645b
metalfortanks <charris.pers@gmail.com>
2022-11-29 19:07:23 +0000

    Form fix

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var submit = document.getElementById("submitButton");
 var form = document.getElementById("becomeFriend");
 submit.addEventListener("click", function(){
-    if(!isValid){
-        e.preventDefault();    //stop form from submitting
-    }
-    if (isValid){
     form.style.visibility = 'hidden';
-    }
 });
